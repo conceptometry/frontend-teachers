5c560f5c9885437e2bcb82d0de1aa95cf7f05831
Yashraj Pahwa <59451283+yashrajpahwa@users.noreply.github.com>
2021-02-11 08:38:45 +0530

    Added redirection to home if user is logged in from login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,41 +1,41 @@
-import Head from "next/head";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { useStateValue } from "../src/context/StateProvider";
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useStateValue } from '../src/context/StateProvider';
 
-import { useCookies } from "react-cookie";
+import { useCookies } from 'react-cookie';
 
 const Login = () => {
   const router = useRouter();
-  const [cookies, setCookie] = useCookies(["token"]);
-  // useEffect(() => {
-  //   if (cookies.token && cookies.token !== null) {
-  //     router.push("/");
-  //   }
-  // }, [cookies.token]);
+  const [cookies, setCookie] = useCookies(['token']);
+  useEffect(() => {
+    if (cookies.token) {
+      router.push('/');
+    }
+  }, [cookies.token]);
 
   const [{ token }, dispatch]: any = useStateValue();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const formData = {
     email,
     password,
   };
 
   useEffect(() => {
-    var forms = document.querySelectorAll(".needs-validation");
+    var forms = document.querySelectorAll('.needs-validation');
 
     // Loop over them and prevent submission
     Array.prototype.slice.call(forms).forEach(function (form) {
       form.addEventListener(
-        "submit",
+        'submit',
         function (event) {
           if (!form.checkValidity()) {
             event.preventDefault();
             event.stopPropagation();
           }
 
-          form.classList.add("was-validated");
+          form.classList.add('was-validated');
         },
         false
       );
@@ -43,16 +43,16 @@ const Login = () => {
   }, []);
 
   const [submitting, setSubmitting] = useState(false);
-  const [response, setResponse] = useState("");
+  const [response, setResponse] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setSubmitting(true);
     const url = `${process.env.NEXT_PUBLIC_API_URI}/auth/login`;
     const options = {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(formData),
     };
@@ -63,17 +63,17 @@ const Login = () => {
       if (resJson.success === true) {
         console.log(resJson);
         dispatch({
-          type: "SET_USER",
+          type: 'SET_USER',
           user: resJson.user,
           token: resJson.token,
         });
         setResponse(`Logged In!`);
         setSubmitting(false);
-        setCookie("token", `${resJson.token}`, {
-          path: "/",
+        setCookie('token', `${resJson.token}`, {
+          path: '/',
         });
-        localStorage.setItem("user", JSON.stringify(resJson.user));
-        router.push("/");
+        localStorage.setItem('user', JSON.stringify(resJson.user));
+        router.push('/');
       } else {
         console.log(resJson.message);
         const message = `${resJson.message}`;
@@ -92,46 +92,46 @@ const Login = () => {
       <Head>
         <title>Conceptometry | Login</title>
       </Head>
-      <div className="container">
+      <div className='container'>
         <img
-          src="/images/logo.webp"
-          alt="Conceptometry Logo"
-          className="mx-auto d-flex"
+          src='/images/logo.webp'
+          alt='Conceptometry Logo'
+          className='mx-auto d-flex'
           style={{
             maxWidth: 300,
           }}
         />
-        <h2 className="text-center">Login</h2>
+        <h2 className='text-center'>Login</h2>
         <form
-          className="mt-4 needs-validation"
+          className='mt-4 needs-validation'
           noValidate={true}
           onSubmit={handleSubmit}
         >
-          <div className="my-3 form-floating">
+          <div className='my-3 form-floating'>
             <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              className="form-control w-100"
+              type='email'
+              name='email'
+              placeholder='Email'
+              className='form-control w-100'
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
             />
-            <label htmlFor="nameField">Email</label>
-            <div className="invalid-feedback">Please provide a valid email</div>
+            <label htmlFor='nameField'>Email</label>
+            <div className='invalid-feedback'>Please provide a valid email</div>
           </div>
-          <div className="my-3 form-floating">
+          <div className='my-3 form-floating'>
             <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              className="form-control w-100"
+              type='password'
+              name='password'
+              placeholder='Password'
+              className='form-control w-100'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
             />
-            <label htmlFor="nameField">Password</label>
-            <div className="invalid-feedback">
+            <label htmlFor='nameField'>Password</label>
+            <div className='invalid-feedback'>
               Please provide a valid password
             </div>
           </div>
@@ -139,20 +139,20 @@ const Login = () => {
             <>
               <button
                 disabled
-                className="btn btn-primary btn-block bg-gradient w-100"
+                className='btn btn-primary btn-block bg-gradient w-100'
               >
                 <span
-                  className="spinner-border spinner-border-sm my-auto mx-auto"
-                  role="status"
-                  aria-hidden="true"
+                  className='spinner-border spinner-border-sm my-auto mx-auto'
+                  role='status'
+                  aria-hidden='true'
                 ></span>
               </button>
             </>
           ) : (
             <>
               <button
-                type="submit"
-                className="btn btn-primary btn-block bg-gradient w-100"
+                type='submit'
+                className='btn btn-primary btn-block bg-gradient w-100'
               >
                 Submit
               </button>
@@ -161,11 +161,11 @@ const Login = () => {
         </form>
         {response ? (
           <>
-            <p className="text-center mt-1">{response}</p>
+            <p className='text-center mt-1'>{response}</p>
           </>
         ) : (
           <>
-            <p className="text-center mt-1">{response}</p>
+            <p className='text-center mt-1'>{response}</p>
           </>
         )}
       </div>
